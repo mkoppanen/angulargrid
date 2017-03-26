5a1c20b14d4dbd2e87424e3bb9d58d16523b08d9
Mikko Koppanen <mikko@kuut.io>
2017-03-26 22:21:58 +0100

    Update angulargrid.js

--- angulargrid.js
+++ angulargrid.js
@@ -321,10 +321,20 @@
               if (options.performantScroll) refreshDomElm(scrollTop);
               if (scope.infiniteScroll) infiniteScroll(scrollTop);
             }
+              
+            function throttle(fn, wait) {
+              var time = Date.now();
+              return function() {
+                if ((time + wait - Date.now()) < 0) {
+                  fn();
+                  time = Date.now();
+                }
+              }
+            }
 
             setTimeout(function() {
               scrollNs.scrollContInfo = getScrollContainerInfo();
-              scrollNs.scrollContInfo.$elm.on('scroll', setInterval(scrollHandler, this.scrollInterval));
+              scrollNs.scrollContInfo.$elm.on('scroll', throttle(scrollHandler, this.scrollInterval));
             }, 0);
 
             //function to get column width and number of columns
