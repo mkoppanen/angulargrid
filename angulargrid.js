8a2de32267061f3a19c0471b8daa1fe3144ce0f7
Mikko Koppanen <mikko@kuut.io>
2017-04-04 14:50:54 +0100

    Update angulargrid.js

--- angulargrid.js
+++ angulargrid.js
@@ -194,7 +194,7 @@
 
               var $elm = options.scrollContainer === 'body' ? win : container;
               return {
-                height: (typeof $elm === 'undefined') ? 0 : ($elm[0].innerHeight || $elm[0].offsetHeight),
+                height: (typeof $elm === 'undefined' || !Array.isArray($elm) || $elm.length < 1) ? 0 : ($elm[0].innerHeight || $elm[0].offsetHeight),
                 scrollHeight: contElm.scrollHeight,
                 startFrom: findPos(domElm, contElm).top,
                 $elm: $elm
